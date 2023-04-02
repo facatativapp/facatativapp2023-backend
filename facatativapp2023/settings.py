d5c2e4cea670746ea146e322976e85743e338ebb
SSCastanedaC <sscastanedac@gmail.com>
2023-04-02 15:46:39 -0500

    fix Agregar localhost a variables de entorno

--- facatativapp2023/settings.py
+++ facatativapp2023/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": env("DATABASE_NAME"),
         "USER": env("DATABASE_USER"),
         "PASSWORD": env("DATABASE_PASS"),
-        "HOST": "localhost",
+        "HOST": env("DATABASE_HOST"),
         "PORT": "3306",
     }
 }
